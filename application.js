3fc9f79321eefebd5cf04a1796d8e4638d00a815
WindBeat <jordanwalker465@gmail.com>
2014-01-31 11:53:17 -0800

    Update application.js

--- application.js
+++ application.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
                  
                  var answer = $('input[type="radio"]:checked').val();
                  
-                 i = (i++)%questions.length;
+                 i = (i+0)%questions.length;
                  
                         if (answer === undefined) {
                                 alert("Select an option.");
