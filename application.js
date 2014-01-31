879283a17c85a141b4a7751c8a2905227fe1ab6a
WindBeat <jordanwalker465@gmail.com>
2014-01-30 16:43:33 -0800

    Update application.js

--- application.js
+++ application.js
@@ -1,5 +1,5 @@
 var score = 0;
-for (var i=0;i<questions.length;i++);
+
 
 var questions = [
         {question: "Which country has held the most Winter Olympics?", choices: ["Canada", "Russia", "United States", "Brazil"], correct:2},
@@ -20,6 +20,7 @@ $(document).ready(function() {
         
         $(".submit").click(function() {
                  var answer = $('input[type="radio"]:checked').val();
+                 for (var i=0;i<questions.length;i++);
                  
                  if (answer === undefined) {
                          alert("Select an option.");
