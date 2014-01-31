1ab0aeaf8f8d887b927723ff3138cda066447ffa
WindBeat <jordanwalker465@gmail.com>
2014-01-31 12:07:23 -0800

    Fixed answer for question 4

--- application.js
+++ application.js
@@ -8,7 +8,7 @@ var questions = [
         {question: "Which country has held the most Winter Olympics?", choices: ["Canada", "Russia", "United States", "Brazil"], correct:2},
         {question: "When did snowboarding start at the Olympics?", choices: [1998, 2005, 1986, 1943], correct:0},
         {question: "Which American has the most gold medals in a single Winter Olympics event?",  choices: ["Shaun White", "Usain Bolt", "Eric Heiden", "Jason Brown"],   correct:2},
-        {question: "What is the oldest event in the Winter Olympics ?",   choices: ["Bobsled", "Snowboarding", "Figure Skating", "Hockey"], correct:1},
+        {question: "What is the oldest event in the Winter Olympics ?",   choices: ["Bobsled", "Snowboarding", "Figure Skating", "Hockey"], correct:3},
         {question: "When were the first Winter Olympic games held?",    choices: [1924, 1989, 2001, 1890], correct:0},
         {question: "What date will the 2014 Winter Olympics begin?",   choices: ["February 1st", "February 7th", "March 5th", "April 23rd"], correct:1},
         {question: "What is the world record speed for downhill skiing?",   choices: ["200mph", "156mph", "40mph", "90mph"], correct:1},
